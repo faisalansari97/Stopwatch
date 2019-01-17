62e948175ab614cc37f58be185803f315e9768e2
faisalansari97 <ansari.faisal343@gmail.com>
2019-01-17 19:16:51 +0530

    Fourth Commit: added logs and lap timing

--- js/demo.js
+++ js/demo.js
@@ -3,25 +3,38 @@
 var time = 0;
 var i = 0;
 var isRunning = false;
-var lap = "00:00:00:00";
+var lap = "00:00:00.00";
+
+// used for synchronization of current time
+var d = new Date();
+const ml = d.getMilliseconds();
+const s = d.getSeconds() * 1000; //convert milliseconds
+const m = d.getMinutes() * 60 * 1000; //convert milliseconds
+const h = d.getHours() * 60 * 60 * 1000; //convert milliseconds
+
 
 
 //to start or stop
 function start() {
+    // capture startTime and save it to log
+    var start = document.getElementById("Start").innerHTML;
+    if(start === "Start"){
+    var startTime = document.getElementById("timer").innerHTML;
+    document.getElementById("startTime").innerHTML += startTime + "</br>";
+    }
     //if clock is stop i.e when start 
     if (!isRunning) {
         isRunning = true;
         increment();
-    }
-    else {
-        isRunning = false;
-    }
-
+        document.getElementById("Start").innerText = "Resume"; 
+    } 
 }
 
-function pause(){
-    isRunning=false;
-
+function stop(){
+    if(isRunning){
+        isRunning = false;   
+    }
+    
 }
 
 //To increase time by 1 every 10 milliseconds
@@ -43,11 +56,10 @@ function increment() {
                 if (hours < 10) { hours = "0" + hours; }
                 if (secs < 10) { secs = "0" + secs; }
 
-                //output  
-                lap = hours + ":" + mins + ":" + secs + ":" + milli;
-
+                //output 
+                 
+                lap = hours + ":" + mins + ":" + secs + "." + milli;
                 document.getElementById("timer").innerHTML = lap;
-
                 increment();
             }, 100)
 
@@ -56,21 +68,46 @@ function increment() {
 
 //to set things to 00:00:00:00    
 function reset() {
+    if(isRunning){
+    //isRunning = true;
+    var stopTime = document.getElementById("timer").innerText;
+    document.getElementById("stopTime").innerHTML += stopTime + "</br>";
+    }
     isRunning = false; 
     time = -1;
     i = 0;
-    document.getElementById("timer").innerHTML = "00:00:00:00";
+    document.getElementById("timer").innerHTML = "00:00:00.00";
+    document.getElementById("Start").innerText = "Start";
+    document.getElementById("lap-times").innerHTML = null; 
+   
 }
 
 function noteLap() {
+    if(isRunning){
         i++;
         let output = "Lap " + i + " on : " + lap;
-        console.log(output);
-        for(let j=0; j<10; j++){
-        let li = document.createElement("li");
-        li.innerText = output;
-        document.getElementById("lap-times").innerHTML = li;
-        j++;
-}
+        console.log(lap);
+        document.getElementById("lap-times").innerHTML += output + "</br>";
+    }
+    else{
+        alert("Run timer and try again!");
+    }
+
 }
 
+function logs(){
+    var display = document.getElementById("history").hidden;
+    if(display === true){
+        document.getElementById("history").hidden = false;
+    }
+   else{
+        document.getElementById("history").hidden = true;
+    }
+
+}
+
+function timeDiff(){
+    
+     
+}
+timeDiff();
